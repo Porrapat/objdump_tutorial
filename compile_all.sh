fd251957719af729067104d25de7ce192679649a
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2020-07-16 22:07:38 +0700

    Add more variable example files

--- compile_all.sh
+++ compile_all.sh
@@ -9,3 +9,9 @@ objdump -d -M intel -S example_left.o > example_left_result.txt
 
 gcc -g -o example_right.o example_right.c
 objdump -d -M intel -S example_right.o > example_right_result.txt
+
+gcc -g -o example_left_add_more_var.o example_left_add_more_var.c
+objdump -d -M intel -S example_left_add_more_var.o > example_left_add_more_var_result.txt
+
+gcc -g -o example_right_add_more_var.o example_right_add_more_var.c
+objdump -d -M intel -S example_right_add_more_var.o > example_right_add_more_var_result.txt
